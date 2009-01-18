84407018d1f50e9f9d1ea4480507a95b4840e12d
xeon <xeon@efed7fae-e156-11dd-ab27-75d9513d8d6c>
2009-01-18 10:36:25 +0000

    build 25

--- Modules/sqlBase/My Project/AssemblyInfo.vb
+++ Modules/sqlBase/My Project/AssemblyInfo.vb
@@ -33,5 +33,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.2007.0")> 
-<Assembly: AssemblyFileVersion("1.0.2007.0")> 
+<Assembly: AssemblyVersion("1.0.2007.25")> 
+<Assembly: AssemblyFileVersion("1.0.2007.25")> 
