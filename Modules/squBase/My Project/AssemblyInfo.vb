43e4d8bc62587ddba5aa604a46ec9dd9536e6dbf
xeon <xeon@efed7fae-e156-11dd-ab27-75d9513d8d6c>
2009-01-18 10:36:25 +0000

    build 25

--- Modules/squBase/My Project/AssemblyInfo.vb
+++ Modules/squBase/My Project/AssemblyInfo.vb
@@ -33,5 +33,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.2009.0")> 
-<Assembly: AssemblyFileVersion("1.0.2009.0")> 
+<Assembly: AssemblyVersion("1.0.2009.25")> 
+<Assembly: AssemblyFileVersion("1.0.2009.25")> 
